0d33b34e8649598ee6f900434122f6d1302f0a3c
musrex <96090997+musrex@users.noreply.github.com>
2022-08-08 16:00:13 -0400

    UI updates

--- main.py
+++ main.py
@@ -11,22 +11,29 @@ from kivy.uix.popup import Popup
 
 import os
 
+from kivy.core.window import Window
+Window.clearcolor = (1, 1, 1, 1)
+
+class Popup(Widget):
+    load = ObjectProperty(None)
+    cancel = ObjectProperty(None)
+    
+
+
 class magicwordApp(App):
     def build(self):
         self = GridLayout(rows=6, cols=1)
-        self.rows = 6
         self.size_hint = (0.8, 0.8)
         self.pos_hint = {'center_y': 0.5, 'center_x': 0.5}
 
         #add widgets to window
 
         # image widget
-        self.add_widget(Image(source="logo.png",
-                                    pos_hint=(2,1)))
-
+        self.add_widget(Image(source="logo.png"))
 
         btn1 = Button(text ="Select File",
                     background_color =(2,2,2,2))
+        btn1.bind(on_release=Popup)
         btn2 = Button(text ="Encrypt",
                     background_color =(2,2,2,2))
         btn3 = Button(text ="Decrypt",

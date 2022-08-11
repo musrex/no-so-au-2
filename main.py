d164e810a980cb0ceceaab4cbfc1c0cf6bd9eb81
musrex <96090997+musrex@users.noreply.github.com>
2022-08-11 13:49:13 -0400

    more fumbling, fileuploader pops up now

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from kivy.uix.image import Image
 from kivy.uix.button import Button
 from kivy.properties import ObjectProperty
 from kivy.uix.popup import Popup
+from kivy.uix.floatlayout import FloatLayout
 from crypt import *
 
 import os
@@ -12,54 +13,55 @@ import os
 from kivy.core.window import Window
 Window.clearcolor = (1, 1, 1, 1)
 
-class LoadDialog(Widget):
-    load = ObjectProperty(None)
-    cancel = ObjectProperty(None)
+class MenuApp(Widget):
+    loadfile = ObjectProperty(None)
+    savefile = ObjectProperty(None)
+    text_input = ObjectProperty(None)
+
+    def dismiss_popup(self):
+        self._popup.dismiss()
+
     def show_load(self):
         content = LoadDialog(load=self.load, cancel=self.dismiss_popup)
         self._popup = Popup(title="Load file", content=content,
                             size_hint=(0.9, 0.9))
         self._popup.open()
 
+    def show_save(self):
+        content = SaveDialog(save=self.save, cancel=self.dismiss_popup)
+        self._popup = Popup(title="Save file", content=content,
+                            size_hint=(0.9, 0.9))
+        self._popup.open()
+
     def load(self, path, filename):
         with open(os.path.join(path, filename[0])) as stream:
-            self.text_input.text = stream.read()
+            self.text_input = stream.read()
+            self.selection = self.text_input
 
         self.dismiss_popup()
 
+    def save(self, path, filename):
+        with open(os.path.join(path, filename), 'w') as stream:
+            stream.write(self.text_input.text)
+
+        self.dismiss_popup()
+
+    
+
+class LoadDialog(FloatLayout):
+    load = ObjectProperty(None)
+    text_input = ObjectProperty(None)
+    cancel = ObjectProperty(None)
+
+
+class SaveDialog(FloatLayout):
+    save = ObjectProperty(None)
+    text_input = ObjectProperty(None)
+    cancel = ObjectProperty(None)
 
 class magicwordApp(App):
-    def build(self):
-        self = GridLayout(rows=6, cols=1)
-        self.size_hint = (0.6, 0.8)
-        self.pos_hint = {'center_y': 0.5, 'center_x': 0.5}
-        self.spacing = 25
-
-        #add widgets to window
-
-        # image widget
-        self.add_widget(Image(source="logo.png"))
-
-        btn1 = Button(text ="Select File",
-                    background_color =(1,0,0,1))
-        btn1.bind(on_release=Popup)
-        btn2 = Button(text ="Encrypt",
-                    background_color =(1,0,0,1))
-        btn2.bind(on_release=encryptFile)
-        btn3 = Button(text ="Decrypt",
-                    background_color =(1,0,0,1))
-        btn3.bind(on_release=decryptFile)
-        btn4 = Button(text ="Settings",
-                    background_color =(1,0,0,1))
-        
-
-        self.add_widget(btn1)
-        self.add_widget(btn2)
-        self.add_widget(btn3)
-        self.add_widget(btn4)
-
-
-        return self
+    def build(self): 
+        return MenuApp()
 
 if __name__ == "__main__":
     magicwordApp().run()
